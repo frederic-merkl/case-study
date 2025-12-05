7f262738aad5dadec2ece896236df936cea9e6e3
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-05 21:37:53 +0100

    finished Company model relations, fillable, casts

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,9 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+use App\Models\User;
+use App\Models\Job;
 
 class Company extends Model
 {
     /** @use HasFactory<\Database\Factories\CompanyFactory> */
     use HasFactory;
+
+    protected $fillable = ["user_id", "name", "description", "city", "street", "zip_code", "country", "email", "employee_size", "tags"];
+    protected $casts = ["tags" => "array"];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }
+
