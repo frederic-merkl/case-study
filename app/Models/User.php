843c4dc242f8305ddad3e9f2ee1cee67e8c07043
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-05 22:06:49 +0100

    feat: finished User model relations fix: missing Job import in Category

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+use App\Models\Company;
+use App\Models\Job;
+
 
 class User extends Authenticatable
 {
@@ -44,5 +49,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+     public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
+     public function companies(): HasMany
+    {
+        return $this->hasMany(Company::class);
     }
 }
