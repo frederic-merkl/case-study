33eba47bfe9315640faa78bec5f6c34bdb79782f
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-05 21:47:18 +0100

    feat: finished category relations, fillable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    protected $fillable = ["name"];
+    
+    public function jobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class);
+    }
 }
