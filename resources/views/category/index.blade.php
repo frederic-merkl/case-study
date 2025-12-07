342f532371665409b84256a5e43383c1d95da52d
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-07 17:43:10 +0100

    feat: add first draft of category view layout

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kategorien-Übersicht</title>
+</head>
+
+<body>
+    <header>
+        <h1>Kategorien</h1>
+        <p>
+            <a href="{{ route("categories.create") }}">Neue Kategorie erstellen</a>
+        </p>
+    </header>
+
+    @if ($categories->isEmpty())
+        <p>Es wurden keine Kategorien gefunden.</p>
+    @else
+        <ul>
+            @foreach ($categories as $category)
+                <li>
+                    <a href="{{ route("categories.show", ["category" => $category]) }}">
+                        {{ $category->name }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+
+    <footer>
+        <hr>
+        <a href="{{ route("categories.index") }}">Kategorien-Übersicht</a>
+        <br>
+        <a href="{{ route("categories.index") }}">Kategorien-Verwaltung</a>
+        <br>
+        <a href="{{ route("companies.index") }}">Firmen-Verwaltung</a>
+    </footer>
+</body>
+
+</html>
