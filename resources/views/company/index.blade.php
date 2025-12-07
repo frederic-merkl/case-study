f23d4f7089d999c6a31d7be683462ca2d9ed6c0b
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-07 17:22:27 +0100

    feat: add first draft of index company view layout

---
+++ resources/views/company/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unternehmens-Übersicht</title>
+</head>
+
+<body>
+    <header>
+        <h1>Unternehmen</h1>
+        <p>
+            <a href="{{ route("companies.create") }}">Neues Unternehmen anlegen</a>
+        </p>
+    </header>
+
+    @if ($companies->isEmpty())
+        <p>Es wurden keine Unternehmen gefunden.</p>
+    @else
+        <ul>
+            @foreach ($companies as $company)
+                <li>
+                    <a href="{{ route("companies.show", ["company" => $company]) }}">
+                        {{ $company->name }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+
+    <footer>
+        <hr>
+        <a href="{{ route("companies.index") }}">Unternehmens-Übersicht</a>
+        <br>
+        <a href="{{ route("jobs.index") }}">Job-Übersicht</a>
+        <br>
+        <a href="{{ route("categories.index") }}">Kategorien-Übersicht</a>
+    </footer>
+</body>
+
+</html>
