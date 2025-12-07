a25c827dedca88a56dea75a917010a3c033e8c76
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-07 21:03:47 +0100

    style: remove company details below header in jobs show view

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $job->title }}</title>
+    <link rel="stylesheet" href="{{ asset('css/jobs.css') }}">
 </head>
 
 <body>
 
     <header>
         <h1>{{ $job->title }}</h1>
-        <p>{{ $job->company }}</p>
         <hr>
     </header>
 
