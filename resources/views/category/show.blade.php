49d3aa1c3f5074172a93f89253b6b09c258921a9
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-07 17:46:25 +0100

    feat: add first draft of category show layout

---
+++ resources/views/category/show.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kategorie: {{ $category->name }}</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Kategorie: {{ $category->name }}</h1>
+    </header>
+
+    <div>
+        ID: {{ $category->id }}
+    </div>
+    <div>
+        Name: {{ $category->name }}
+    </div>
+    <div>
+        Erstellt: {{ $category->created_at }}
+    </div>
+    <div>
+        Aktualisiert: {{ $category->updated_at }}
+    </div>
+
+    <footer>
+        <hr>
+        <a href="{{ route('categories.edit', $category) }}">Bearbeiten</a>
+        <br>
+        <a href="{{ route('categories.index') }}">Zur Übersicht</a>
+    </footer>
+
+</body>
+
+</html>
