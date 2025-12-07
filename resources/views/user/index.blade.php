679628674935f29d2cea3a07fe173c96b7c3f05b
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-07 17:55:20 +0100

    feat: add first draft of user index layout

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Benutzer</title>
+</head>
+
+<body>
+	<header>
+		<h1>Benutzer</h1>
+		<p>
+			<a href="{{ route('users.create') }}">Neuen Benutzer erstellen</a>
+		</p>
+	</header>
+
+	@if ($users->isEmpty())
+		<p>Es wurden keine Benutzer gefunden.</p>
+	@else
+		<ul>
+			@foreach ($users as $user)
+				<li>
+					<div>
+						ID: {{ $user->id }}
+					</div>
+					<div>
+						Name: <a href="{{ route('users.show', $user) }}">{{ $user->name }}</a>
+					</div>
+					<div>
+						E-Mail: {{ $user->email }}
+					</div>
+					<div>
+						Erstellt: {{ $user->created_at }}
+					</div>
+					<div>
+						Aktualisiert: {{ $user->updated_at }}
+					</div>
+					<div>
+						<a href="{{ route('users.edit', $user) }}">Bearbeiten</a>
+					</div>
+				</li>
+			@endforeach
+		</ul>
+	@endif
+
+	<footer>
+		<hr>
+		<a href="{{ route('users.index') }}">Benutzer-Übersicht</a>
+		<br>
+	</footer>
+</body>
+
+</html>
