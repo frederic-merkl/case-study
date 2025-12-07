527f6b8b08692e047be1ae74fc67462d15ee9801
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-07 18:01:43 +0100

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('jobs/index');
 });
+
+// Resource routes with implicit model binding
+Route::resource('jobs', JobController::class);
+Route::resource('companies', CompanyController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('users', UserController::class);
