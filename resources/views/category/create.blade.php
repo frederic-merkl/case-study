75748a319d3a711ed6df4cdb143036623785450b
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-07 17:26:30 +0100

    feat: add first draft of create category view layout

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Neue Kategorie anlegen</title>
+</head>
+
+<body>
+	<div>
+		<form action="{{ route('categories.store') }}" method="POST">
+
+			<h2>Neue Kategorie anlegen</h2>
+
+			<div>
+				<label for="name">Name</label>
+				<input id="name" type="text" name="name" required>
+			</div>
+
+			<div>
+				<button type="submit">Speichern</button>
+				<a href="{{ route('categories.index') }}">Abbrechen</a>
+			</div>
+		</form>
+	</div>
+</body>
+
+</html>
