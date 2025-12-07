615dba70f152e2e892b5bb1dee7bb7d3e64560d8
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-07 17:58:23 +0100

    feat: add first draft of user create layout

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Neuen Benutzer anlegen</title>
+</head>
+
+<body>
+	<div>
+		<form action="{{ route('users.store') }}" method="POST">
+			@csrf
+
+			<h2>Neuen Benutzer anlegen</h2>
+
+			<div>
+				<label for="name">Name</label>
+				<input id="name" type="text" name="name" required>
+			</div>
+
+			<div>
+				<label for="email">E-Mail</label>
+				<input id="email" type="email" name="email" required>
+			</div>
+
+			<div>
+				<label for="password">Passwort</label>
+				<input id="password" type="password" name="password" required>
+			</div>
+
+			<div>
+				<label for="password_confirmation">Passwort bestätigen</label>
+				<input id="password_confirmation" type="password" name="password_confirmation" required>
+			</div>
+
+			<div>
+				<button type="submit">Speichern</button>
+				<a href="{{ route('users.index') }}">Abbrechen</a>
+			</div>
+		</form>
+	</div>
+</body>
+
+</html>
