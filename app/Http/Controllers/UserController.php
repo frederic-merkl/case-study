3e5c80b7cfd684247a5bc142d4ddaa8cbaffd169
Frederic Merkl <fredericmerkl@outlook.com>
2025-12-11 22:20:53 +0100

    fix: add ignore parameter for user update in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,7 +63,7 @@ class UserController extends Controller
     {
         $validated = $request->validate([
             "name" => "required|string|max:255",
-            "email" => "required|email|unique:users,email,",
+            "email" => "required|email|unique:users,email,{$user->id}",
             "password" => "nullable|min:8",
         ]);
 
