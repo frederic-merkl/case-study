337e5283b7242103553aeba6eef85f322b06db65
Frederic Merkl <fredericmerkl@outlook.com>
2026-01-12 23:49:17 +0100

    feat: add auth middleware to job and company routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,32 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return redirect()->route('jobs.index');
+
+
+// login routes
+
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::post('/login', [AuthController::class, 'store'])->name('login.store');
+
+Route::middleware('auth')->group(function () {
+    // regular view
+    Route::get('/', function () {
+        return redirect()->route('jobs.index');
+    });
+
+    // CRUD for all models
+    Route::resource('jobs', JobController::class);
+    Route::resource('companies', CompanyController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('users', UserController::class);
+
+
+
 });
 
-// Resource routes with implicit model binding
-Route::resource('jobs', JobController::class);
-Route::resource('companies', CompanyController::class);
-Route::resource('categories', CategoryController::class);
-Route::resource('users', UserController::class);
